39c354818ddb0798d7f6cef0d04de90caf8ea532
Isikyus <isikyus@users.noreply.github.com>
2018-08-15 23:22:07 +1000

    Parse Dwarf Fortress rhythm strings
    Now at least we can hear what the (non-polyrhythm) beats sound like

--- cacophony.rb
+++ cacophony.rb
@@ -49,11 +49,60 @@ class Cacophony
 	end
 end
 
+# Knows how to parse Dwarf Fortress rhythm notation, like | x x - X |
+class Rhythm
+
+	# Amplitude values for each Dwarf Fortress beat symbol.
+	# These are in no particular scale; the maximum volume will be whatever's loudest in any particular string.
+	BEAT_VALUES = {
+
+		# Silence
+		'-' => 0,
+
+		# Regular beat
+		'x' => 4,
+
+		# Accented beat
+		'X' => 5,
+
+		# Primary accent
+		'!' => 8
+	}
+
+	BAR_LINE = '|'
+
+	def initialize(rhythm_string)
+		@amplitudes = parse(rhythm_string)
+	end
+
+	def each_beat(&block)
+		@amplitudes.each(&block)
+	end
+
+	private
+
+	def parse(rhythm_string)
+
+		# TODO: should I be ignoring bar lines? Is there anything I can do with them?
+		raw_amplitudes = rhythm_string.split(' ').reject { |char| char == BAR_LINE }.map do |char|
+			BEAT_VALUES[char] || raise("Unknown beat symbol #{char}")
+		end
+
+		# Ensure all our amplitudes are between 0.0 and 1.0
+		highest_volume = raw_amplitudes.max
+		raw_amplitudes.map { |amplitude| amplitude.to_f / highest_volume }
+	end
+end
 
 caco = Cacophony.new
 
+# From http://www.bay12games.com/dwarves/early_musical_forms.html
+agek = Rhythm.new('| - x x - X - - - | x - - x x ! x - | X x x - x - - - | - X x - x - x - |')
+
 3.times do
-	caco.add_note(440, 1, 0.5)
-	caco.add_note(440, 0.5, 0.5)
+	agek.each_beat do |amplitude|
+		p amplitude
+		caco.add_note(440, amplitude, 0.5)
+	end
 end
 caco.write("sound.wav")
