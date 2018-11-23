c2311fa26d250a8ddefe82836df4eeda8f07b0d5
Isikyus <isikyus@users.noreply.github.com>
2018-11-23 23:14:07 +1100

    Add gold-master spec for early and late beats

--- spec/integration/gold_master_spec.rb
+++ spec/integration/gold_master_spec.rb
@@ -3,14 +3,41 @@ require 'open3'
 
 RSpec.describe 'Inevitable Cacophony' do
 
-	specify 'generates a known file in 4/4 time' do
-		known_data = File.open('spec/fixtures/4-4.wav').read
-		score = '| x X x ! |'
+	context 'generating known files from given scores' do
+		let(:known_data) { File.open(fixture_file).read }
+		let(:generated_data) do
+			data, status = Open3.capture2('ruby', '-Ilib', 'cacophony.rb', score)
+			expect(status).to eq 0
+			data
+		end
 
-		# Command here should match README.
-		generated_data, status = Open3.capture2("ruby -Ilib cacophony.rb '#{score}'")
+		context 'in 4/4 time' do
+			let(:score) { '| x X x !' }
+			let(:fixture_file) { 'spec/fixtures/4-4.wav' }
 
-		expect(status).to eq 0
-		expect(generated_data).to eq known_data
+			specify 'works' do
+				expect(generated_data).to eq known_data
+			end
+		end
+
+		context 'using early and late beats' do
+			let(:score) { "| x x'x`x |" }
+			let(:fixture_file) { 'spec/fixtures/1-and-3.wav' }
+
+			specify 'works' do
+				expect(generated_data).to eq known_data
+			end
+		end
+
+		specify 'generates a known file in 4/4 time' do
+			known_data = File.open('spec/fixtures/4-4.wav').read
+			score = '| x X x ! |'
+
+			# Command here should match README.
+			generated_data, status = Open3.capture2("ruby -Ilib cacophony.rb '#{score}'")
+
+			expect(status).to eq 0
+			expect(generated_data).to eq known_data
+		end
 	end
 end
