7843c5cce3a46f9b0835d0b83c5d0221b5df8c5c
Isikyus <isikyus@users.noreply.github.com>
2019-07-08 23:59:23 +1000

    [CI skip] Skeleton of rhythm-text parser and test
    Nowhere near working, but it's enough to focus the actual work.

---
+++ lib/parser/rhythms.rb
@@ -0,0 +1,16 @@
+# Knows how to parse the rhythm-description paragraphs
+# of a Dwarf Fortress musical form
+
+module Parser
+	class Rhythms
+
+		# Parses the rhythms from the given form text.
+		# TODO: is this the whole text or just the bit about rhythms?
+		#
+		# @param form_text [String]
+		# @return [Hash{Symbol,Rhythm}]
+		def parse(form_text)
+			{}
+		end
+	end
+end
