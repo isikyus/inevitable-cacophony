cc16d93aa2ed68e05c2e47e8148bc4444422550b
Isikyus <isikyus@users.noreply.github.com>
2020-06-08 12:04:38 +1000

    Switch to using JSON library for export
    And begin exporting musical form vocals in a better format.
    As I do this I'm realising the data format isn't fully mapped yet,
    so I may end up needing to contribute to the field analysis to get
    this to work properly — but hopefully the basic data Cacophony already
    uses will be there.

--- export-music.lua
+++ export-music.lua
@@ -9,52 +9,69 @@ Exports all musical forms as JSON, for use with Inevitable Cacophony
 
 ]====]
 
-local function format_bool(boolean)
-  if boolean
-  then
-    return 'false'
-  else
-    return 'true'
-  end
+json = require('json')
+
+local function MapList(lookups, list)
+    local listData = {}
+    for i, entry in ipairs(list) do
+        listData[i] = lookups[entry]
+    end
+
+    return listData
 end
 
--- Print a JSON list.
-local function print_list(list)
-  print('[')
-  for i, value in ipairs(list) do
-    print('  '..value..',')
-  end
-  print('0]')
+local function VocalsData(vocals)
+    local vocalsData = {}
+
+    for i, voice in ipairs(vocals) do
+        local voiceData = {}
+
+        voiceData['vocal_components'] = MapList(
+            { 'melody', 'counterpoint', 'harmony', 'rhythm' },
+            voice.vocal_components
+        )
+        voiceData['phrase_lengths'] = MapList(
+            { 'short', 'mid-length', 'long', 'varied-length' },
+            voice.phrase_lengths
+        )
+
+        vocalsData[i] = voiceData
+    end
+
+    return vocalsData
 end
 
-local function print_json_vocals(vocals)
-  print('    "scales":[')
-  for i, voice in ipairs(vocals) do
-    print('      {')
-    print('        "vocal_components":')
-    print_list(voice.vocal_components)
-    print('        "phrase_lengths":')
-    print_list(voice.phrase_lengths)
-    print('      },')
-  end
-  print('{}]')
-end
-
-print('[')
-for i,v in ipairs(df.global.world.musical_forms.all) do
-    local name = dfhack.TranslateName(v.name, 1)
+local function MusicalFormsData()
     local style = df.musical_form_style
+    local formsData = {}
 
-    print('  {')
-    print('    "name":"'..name..'",')
-    print('    "tempo_style":"'..style[v.tempo_style]..'",')
-    print('    "dynamic_style":"'..style[v.dynamic_style]..'",')
-    print('    "pitch_style":"'..style[v.pitch_style]..'",')
-    print('    "overall_style":"'..style[v.overall_style]..'",')
-    print('    "purpose":"'..df.musical_form_purpose[v.purpose]..'",')
-    print('    "produces_individual_songs":'..format_bool(v.flags.produces_individual_songs)..',')
-    print('    "repeats_as_necessary":'..format_bool(v.flags.repeats_as_necessary)..',')
-    print_json_vocals(v.scales)
-    print('  },')
+    for i,v in ipairs(df.global.world.musical_forms.all) do
+        local form = {}
+
+        form['name'] = dfhack.TranslateName(v.name, 1)
+        form['tempo_style'] = style[v.tempo_style]
+        form['dynamic_style'] = style[v.dynamic_style]
+        form['pitch_style'] = style[v.pitch_style]
+        form['overall_style'] = style[v.overall_style]
+        form['purpose'] = df.musical_form_purpose[v.purpose]
+        form['produces_individual_songs'] = v.flags.produces_individual_songs
+        form['repeats_as_necessary'] = v.flags.repeats_as_necessary
+        form['vocals'] = VocalsData(v.scales)
+
+        formsData[i] = form
+    end
+
+    return formsData
 end
-print('{}]')
+
+function ExportForms()
+    local savePath = dfhack.getSavePath()
+    local filename = savePath .. "/musical-forms.json"
+    local file = json.open(filename)
+    file.data = MusicalFormsData()
+    file:write()
+
+    print('Exported musical forms to ' .. filename)
+end
+
+ExportForms()
